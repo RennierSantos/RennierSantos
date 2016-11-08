6da8f43a678087d385c6c079a5791ccb431326a1
RennierSantos <lottrennier@gmail.com>
2016-11-08 19:52:19 -0200

    alterado

---
+++ proj1-erin/src/projeto/NotaFiscal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class NotaFiscal {
+    
+    private int numero;
+    private String data;
+    private String CNPJ;
+    
+    
+    
+    
+    
+}
